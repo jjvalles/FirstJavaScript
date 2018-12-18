7aa05ab4a1a06a3359c6e86fe4b1c1dc35a5c5a9
Jacob Valles (Student) <jacob.v595@slcstudents.org>
2018-12-18 08:23:33 -0700

    added variable stuff

--- myScript.js
+++ myScript.js
@@ -1,3 +1,18 @@
+//This function changes the text in the demo paragraph
+
 function myFunction () {
     document.getElementById('demo').innerHTML = "Paragraph Changed";
+}
+
+//Defining variables
+
+var x, y, z;
+x = 5;
+y = 6;
+z = x + y;
+
+//This function changes the text in the math paragraph
+
+function mathFunction () {
+    document.getElementById('math').innerHTML = z;
 }
